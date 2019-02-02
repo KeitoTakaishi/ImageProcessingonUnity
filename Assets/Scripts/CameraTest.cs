2b141799059beaaa5cdc9d1e543d280e2781df16
takaishi keito <keitoncis@icloud.com>
2019-02-02 22:42:47 +0900

    display 16:9

--- Assets/Scripts/CameraTest.cs
+++ Assets/Scripts/CameraTest.cs
@@ -6,20 +6,58 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class CameraTest : MonoBehaviour {
-	[SerializeField] private int m_width = 1280;
-	[SerializeField] private int m_height = 720;
+	[SerializeField] private int m_width;
+	[SerializeField] private int m_height;
 	[SerializeField] private int FPS;
 	
-	[SerializeField] private Material WebCamMat;	//撮影中
-	[SerializeField] private Material ResultMat;	//結果画像
+	[SerializeField] private Material WebCamMat;	
+	[SerializeField] private Material ResultMat;	
 	
 	private WebCamTexture webcamTexture = null;
 	private int id = 0;
 
-	public GameObject canv;
+	public GameObject TakePicScene;
+	public GameObject ResultScene;
+
+
+	#region MonoFunc
+
+	void Awake()
+	{
+		TakePicScene.SetActive(true);
+		ResultScene.SetActive(false);
+	}
 
 	void Start()
 	{
+		WebCamInit();
+	}
+	
+	void Update() {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			TakePicture();
+		}
+		else if (Input.GetKeyDown(KeyCode.A))
+		{
+			ShowPicture();
+		}
+	}
+
+	#endregion
+
+
+	#region MyFunc
+
+
+	void WebCamInit()
+	{
+		
+		//m_width = (int)TakePicScene.GetComponent < RectTransform >().rect.width;
+		//m_height = (int)TakePicScene.GetComponent<RectTransform>().rect.height;
+		
+		m_width = 1280;
+		m_height = 720;
+		
 		WebCamDevice[] devices = WebCamTexture.devices;
 		for (int i = 0; i < devices.Length; i++)
 		{
@@ -36,27 +74,35 @@ public class CameraTest : MonoBehaviour {
 			}
 		}
 	}
-	
-	void Update() {
-		if (Input.GetKeyDown (KeyCode.Space)) {
-			if ( webcamTexture != null ) {
-				SaveToPNGFile( webcamTexture.GetPixels(), Application.dataPath + "/../SavedScreen" + id.ToString() + ".png" );
-				//SaveToPNGFile( webcamTexture.GetPixels(), Application.dataPath + "/../SavedScreen.png" );
-				id++;
-			}
-		}//結果画像をmaterialに表示
-		else if (Input.GetKeyDown(KeyCode.A))
-		{
-			Texture tex = ReadTexture(Application.dataPath + "/../SavedScreen" + (id-1).ToString() + ".png", m_width, m_height);
-			//Texture tex = ReadTexture(Application.dataPath + "/../SavedScreen.png", m_width, m_height);
-			ResultMat.mainTexture = tex;
-			canv.GetComponent < RawImage >().texture = ResultMat.mainTexture;
+
+	public void TakePicture()
+	{
+		if ( webcamTexture != null ) {
+			//change UI
+			TakePicScene.SetActive(true);
+			ResultScene.SetActive(false);
+			//process
+			SaveToPNGFile( webcamTexture.GetPixels(), Application.dataPath + "/../SavedScreen" + id.ToString() + ".png" );
+			id++;
 		}
 	}
+
+
+	public void ShowPicture()
+	{
+		//change UI
+		TakePicScene.SetActive(!TakePicScene.active);
+		ResultScene.SetActive(!ResultScene.active);
+		Texture tex = ReadTexture(Application.dataPath + "/../SavedScreen" + (id-1).ToString() + ".png", m_width, m_height);
+		ResultMat.mainTexture = tex;
+		
+		ResultScene.GetComponent < RawImage >().texture = ResultMat.mainTexture;
+	}
 	
-	//撮影画像をpngを保存
+	
 	void SaveToPNGFile( Color[] texData , string filename ) {
-		Texture2D takenPhoto = new Texture2D(m_width, m_height, TextureFormat.ARGB32, false);            
+		Texture2D takenPhoto = new Texture2D(m_width, m_height, TextureFormat.ARGB32, false);
+		Debug.Log(takenPhoto.GetPixels().Length);
 		takenPhoto.SetPixels(texData);
 		takenPhoto.Apply();            
 		byte[] png = takenPhoto.EncodeToPNG();
@@ -65,7 +111,7 @@ public class CameraTest : MonoBehaviour {
 		File.WriteAllBytes(filename, png);
 	}
 	
-	//撮影画像
+	
 	byte[] ReadPngFile(string path)
 	{
 		FileStream fileStream = new FileStream(path, FileMode.Open, FileAccess.Read);
@@ -84,4 +130,7 @@ public class CameraTest : MonoBehaviour {
             
 		return textute;
 	}
+
+	#endregion
+	
 }
